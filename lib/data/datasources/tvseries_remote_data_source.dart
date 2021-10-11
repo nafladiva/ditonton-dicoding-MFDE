63960a0a8419f6367a504b226c56e61f989759a2
Nafla Diva Syafia <nafladiva@gmail.com>
2021-10-11 10:35:29 +0700

    initial commit

---
+++ lib/data/datasources/tvseries_remote_data_source.dart
@@ -0,0 +1,17 @@
+abstract class TvSeriesRemoteDataSource {
+  Future<List<TvSeriesModel>> getNowPlayingTvSeries();
+  Future<List<TvSeriesModel>> getPopularTvSeries();
+  Future<List<TvSeriesModel>> getTopRatedTvSeries();
+  Future<> getTvSeriesDetail(int id); //masi kurang
+  Future<List<TvSeriesModel>> getTvSeriesRecommendations();
+  Future<List<TvSeriesModel>> searchTvSeries(String query);
+}
+
+class TvSeriesRemoteDataSourceImpl implements TvSeriesRemoteDataSource {
+  static const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
+  static const BASE_URL = 'https://api.themoviedb.org/3/tv';
+
+  final http.Client client;
+
+  TvSeriesRemoteDataSourceImpl({required this.client});
+}
